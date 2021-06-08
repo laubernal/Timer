311ef18dfc64eb34ced8891aaab79a670508d362
laubernal <lbernalrodrguez@gmail.com>
2021-06-08 20:34:21 +0200

    timer finished

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const timer = new Timer('#durationInput', '#startButton', '#pauseButton');
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter)
+
+let duration;
+const timer = new Timer('#durationInput', '#startButton', '#pauseButton', {
+    onStart(totalDuration) {
+        duration = totalDuration;
+    },
+
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset', 
+            perimeter * timeRemaining / duration - perimeter
+        );
+    },
+
+    onComplete() {
+        
+    }
+});
--- timer.js
+++ timer.js
@@ -1,24 +1,53 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = document.querySelector(durationInput);
         this.startButton = document.querySelector(startButton);
         this.pauseButton = document.querySelector(pauseButton);
+
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
 
     start = () => {
+        if (this.onStart) {
+            this.onStart(this.timeRemaining);
+        }
+        this.tick();
         if (!this.interval) {
-            this.interval = setInterval(this.tick, 1000);
+            this.interval = setInterval(this.tick, 20);
         }
     }
+
     pause = () => {
         clearInterval(this.interval);
         this.interval = undefined;
     }
 
     tick = () => {
-        this.durationInput.value = this.durationInput.value - 1;
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - .02;
+            if (this.onTick) {
+                this.onTick(this.timeRemaining);
+            }
+        }
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time.toFixed(2);
     }
 }
